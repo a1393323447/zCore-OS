309f90cb4e0780744e931c1d4cba8f5963cd2e89
朕与将军解战袍 <1393323447@qq.com>
2023-08-05 15:40:33 +0800

    完善 spin_lock

--- shared/lock.zig
+++ shared/lock.zig
@@ -1,31 +1,36 @@
 const atomic = @import("std").atomic;
 
 pub const SpinLock = struct {
-    locked: atomic.Atomic(usize),
+    locked: atomic.Atomic(bool),
 
     const Self = @This();
 
+    const UNLOCKED: bool = false;
+    const LOCKED: bool = true;
+
     pub fn init() Self {
         return Self{
-            .locked = atomic.Atomic(usize).init(0),
+            .locked = atomic.Atomic(bool).init(UNLOCKED),
         };
     }
 
     pub fn acquire(self: *Self) void {
-        while (true) {
-            const res = self.locked.compareAndSwap(
-            0, 1, atomic.Ordering.SeqCst, atomic.Ordering.SeqCst);
-            if (res) |_| {
-                break;
+        while (self.locked.compareAndSwap(
+            UNLOCKED,
+            LOCKED, 
+            atomic.Ordering.Acquire, 
+            atomic.Ordering.Monotonic)) |_| {
+            while (self.locked.load(atomic.Ordering.Monotonic) == LOCKED) {
+                // spin
             }
         }
     }
 
     pub fn release(self: *Self) void {
-        self.locked.storeUnchecked(0);
+        self.locked.store(UNLOCKED, atomic.Ordering.Release);
     }
 
     pub fn holding(self: *Self) bool {
-        return self.locked.loadUnchecked() == 1;
+        return self.locked.load(atomic.Ordering.Monotonic);
     }
 };
