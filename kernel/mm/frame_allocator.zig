374d47cb5710616071e9a388c95247c0c600058b
a1393323447 <1393323447@qq.com>
2024-02-23 20:28:11 +0800

    backup code

--- kernel/mm/frame_allocator.zig
+++ kernel/mm/frame_allocator.zig
@@ -1,4 +1,10 @@
+const std = @import("std");
+const shared = @import("shared");
+const config = @import("../config.zig");
 const addr = @import("address.zig");
+const panic = @import("../panic.zig");
+
+const ArrayList = std.ArrayList;
 
 pub const FrameTracker  = struct {
     ppn: addr.PhysPageNum,
@@ -11,15 +17,65 @@ pub const FrameTracker  = struct {
     }
 
     pub fn deinit(self: *const Self) void {
-        
+        frame_dealloc(self.ppn);
     }
 };
 
+pub const StackFrameAllocator = struct {
+    current: usize,
+    end: usize,
+    recycled: ArrayList(usize),
+
+    const Self = @This();
+    pub fn init(l: addr.PhysPageNum, r: addr.PhysPageNum, allocator: std.mem.Allocator) Self {
+        return Self {
+            .current = l.v,
+            .end = r.v,
+            .recycled = ArrayList(usize).init(allocator),
+        };
+    }
+
+    pub fn alloc(self: *Self) ?addr.PhysPageNum {
+        if (self.recycled.popOrNull()) |ppn| {
+            return addr.PhysPageNum.from(ppn);
+        } else {
+            if (self.current == self.end) {
+                return null;
+            } else {
+                self.current += 1;
+                return addr.PhysPageNum.from(self.current - 1);
+            }
+        }
+    }
+
+    pub fn dealloc(self: *Self, ppn: addr.PhysPageNum) void {
+        if (ppn.v >= self.current) {
+            panic.panic("Frame ppn = {x} has not been allocated!", .{ppn.v});
+        }
+        self.recycled.append(ppn) catch unreachable;
+    }
+};
+
+var FRAME_ALLOC_LOCK = shared.lock.SpinLock.init();
+var FRAME_ALLOCATOR: StackFrameAllocator = undefined;
+
+extern fn ekernel() noreturn;
+
+pub fn init_frame_allocator(allocator: std.mem.Allocator) void {
+    const ekernel_addr = addr.PhysAddr.from(@intFromPtr(&ekernel)).ceil();
+    const mem_end_addr = addr.PhysAddr.from(config.MEMORY_END).ceil();
+    FRAME_ALLOCATOR = StackFrameAllocator.init(ekernel_addr, mem_end_addr, allocator);
+}
+
 pub fn frame_alloc() ?FrameTracker {
-    return null;
+    FRAME_ALLOC_LOCK.acquire();
+    defer FRAME_ALLOC_LOCK.release();
+    return FRAME_ALLOCATOR.alloc();
 }
 
 pub fn frame_dealloc(ppn: addr.PhysPageNum) void {
-    
+    FRAME_ALLOC_LOCK.acquire();
+    defer FRAME_ALLOC_LOCK.release();
+    FRAME_ALLOCATOR.dealloc(ppn);
 }
 
