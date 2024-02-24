680d3322979677e6a0eb17ab44ce1dba4aa8085c
朕与将军解战袍 <1393323447@qq.com>
2024-02-24 18:03:32 +0800

    panic 中不再使用 sbi 退出

--- kernel/panic.zig
+++ kernel/panic.zig
@@ -3,5 +3,9 @@ const console = @import("console.zig");
 
 pub fn panic(comptime fmt: []const u8, args: anytype) noreturn {
     console.logger.err(fmt, args);
-    riscv.sbi.shutdown();
+
+    // 这里不能使用 sbi 进行退出
+    while (true) {
+        asm volatile ("wfi");
+    }
 }
